2ceec415401c391a8b145150ea94c9249e39a9ef
Charline Murphy <charlineee@users.noreply.github.com>
2022-06-17 09:09:51 -0700

    onClick listener
    added listener to textviews
    displaying country id onclick from hashmap

--- app/src/main/java/com/example/homeworkproject/adapter/LocationAdapter.java
+++ app/src/main/java/com/example/homeworkproject/adapter/LocationAdapter.java
@@ -1,6 +1,7 @@
 package com.example.homeworkproject.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +14,15 @@ import com.example.homeworkproject.R;
 import com.example.homeworkproject.model.Country;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHolder> {
 
     private ArrayList<Country> countryDataArrayList;
     Context context;
+    private HashMap<String, Integer> map = new HashMap<>();
+    Integer id = 0;
 
     public LocationAdapter(ArrayList<Country> countryArrayList, Context context) {
         this.countryDataArrayList = countryArrayList; //from main
@@ -36,7 +40,7 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Country model = countryDataArrayList.get(position);
         holder.countryText.setText(model.getCountryName());
-
+        map.put(model.getCountryName(), model.getCountryId());
     }
 
     @Override
@@ -52,6 +56,14 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHo
             super(itemView);
 
             countryText= itemView.findViewById(R.id.country);
+            countryText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String tvText = countryText.getText().toString();
+                    id = map.get(tvText);
+                    Log.d("Tag", "onClick: " + id);
+                }
+            });
         }
     }
 }
